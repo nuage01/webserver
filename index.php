f5eb40fe582b2f9a04638560976a5bc823f78c32
lyes-intern <lyes.khalfaoui@dataimpact.fr>
2020-09-21 17:44:42 +0200

    index

--- index.php
+++ index.php
@@ -8,6 +8,9 @@
 <form action ="<?php 
 session_start();
 $bdd_username = array("lyes" => "lyes01", "antoine" => "antoine02", "amjad" => "amjad03", "louis" => "louis03");
+echo($_POST['password']);
+echo($bdd_username[$_POST['username']);
+
 if (in_array($_POST['username'], $bdd_username ) && $bdd_username[$_POST['username']] == $_POST['password']) {header("location: main.php"); }
 else {}
   ?>"  method ="POST">
