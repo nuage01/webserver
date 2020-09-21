d67df40c8cf8959a6dc35185b629bb417f5db0a6
lyes-intern <lyes.khalfaoui@dataimpact.fr>
2020-09-21 17:39:40 +0200

    improve

--- index.php
+++ index.php
@@ -7,11 +7,9 @@
 
 <form action ="<?php 
 session_start();
-$bdd_username = array("lyes", "antoine", "amjad", "louis");
-$bdd_password = array("azerty", "123456");
-if (in_array($_POST['username'], $bdd_username )) {header("location: main.php"); }
-else {exit;
-     echo 'WRONG IDS';}
+$bdd_username = array("lyes" => "lyes01", "antoine" => "antoine02", "amjad" => "amjad03", "louis" => "louis03");
+if (in_array($_POST['username'], $bdd_username ) && $bdd_username[$_POST['username']] == $_POST['password']) {header("location: main.php"); }
+else {}
   ?>"  method ="POST">
 <input type ="text" name="username" value="username" >
 <input type ="text" name="password" value ="password" >
