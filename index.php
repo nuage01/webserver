12be8ca19c2ea9315ba4afe9b8aef0c9fb02b2bd
lyes-intern <lyes.khalfaoui@dataimpact.fr>
2020-09-21 17:58:37 +0200

    index

--- index.php
+++ index.php
@@ -8,11 +8,12 @@
 <form action ="<?php 
 session_start();
 $bdd_username = array("lyes" => "lyes01", "antoine" => "antoine02", "amjad" => "amjad03", "louis" => "louis03");
-echo($_POST['password']);
-echo($bdd_username[$_POST['username']);
 
-if (in_array($_POST['username'], $bdd_username ) && $bdd_username[$_POST['username']] == $_POST['password']) {header("location: main.php"); }
-else {}
+
+if (in_array($_POST['username'], $bdd_username ) || $bdd_username[$_POST['username']] == $_POST['password']) {header("location: main.php"); }
+else {echo <<< EOT
+    FAKE LOGINS
+EOT;}
   ?>"  method ="POST">
 <input type ="text" name="username" value="username" >
 <input type ="text" name="password" value ="password" >
