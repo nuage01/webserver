cfcd331b9ba5545ead25a142ff18e6654e567bdc
lyes-intern <lyes.khalfaoui@dataimpact.fr>
2020-10-05 17:20:52 +0200

    add new sql connection

--- index.php
+++ index.php
@@ -9,14 +9,23 @@
 session_start();
 $bdd_username = array("lyes" => "lyes01", "antoine" => "antoine02", "amjad" => "amjad03", "louis" => "louis03");
 
+try{
+  $base = new PDO('mysql:host=localhost;dbname=utilisateurs','root','data01');
+} catch (Exception $e){
+  die('Erreur : ' . $e->getMessage());
+}
+
+$reponse = $base->query('select login as l, password as p from  USERS where  Pays is not NULL');
+while($ligne = $reponse->fetch()){
+  echo ($ligne);
+
+ }
 
 if (in_array($_POST['username'], $bdd_username ) && $bdd_username[$_POST['username']] == $_POST['password']) {header("location: main.php"); }
-else {echo <<< EOT
-    FAKE LOGINS
-EOT;}
+else {}
   ?>"  method ="POST">
 <input type ="text" name="username" value="username" >
-<input type ="text" name="password" value ="password" >
+<input type ="password" name="password" value ="password" >
 <input type ="submit" value="LOGIN">
 
 
